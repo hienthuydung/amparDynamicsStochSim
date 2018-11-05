b3afcbd0859ea2179ec22f6483f03be715035e65
hienthuydung <37580465+hienthuydung@users.noreply.github.com>
2018-11-05 19:01:50 +0100

    Update coefficientOfVariation.py

--- coefficientOfVariation.py
+++ coefficientOfVariation.py
@@ -1,6 +1,6 @@
 # Plotting CV from multiple simulations
 
-# This program was used to create Fig. 4.1 and 4.3 of the manuscript.
+# This program was used to create Fig. Fig. 4.1(a),(b) and Fig. 4.2(a),(b) of the manuscript.
 # It plots the CV of w_i of 7 synapses from multiple simulations and combines them into one figure.
 # To get the data for the CV please run stochasticSimulation.py
 
@@ -36,11 +36,13 @@ for i in range(1,4):
     
 regression = []
 x_axis_cv = []
-F = [0.5,0.7,0.9] # set F here for scaling the x axis (F*s_i)
+F = [0.5,0.7,0.9] # set F here for scaling the x axis like $w_i^\infty=F*s_i$
 x_axis_regression = np.linspace(0.0,120,500,endpoint=True)
 
 for i in range(1,4):
-    x_axis_i = np.array([1,2,5,10,20,50,100])*F[i-1]
+    x_axis_i = np.array([1,2,5,10,20,50,100])*F[i-1] # for Fig. 4.1,4.2(b) 
+        # if you want Fig. 4.1,4.2(a): 
+        # x_axis_i = np.array([1,2,5,10,20,50,100])*F[i-1]
     av_x_axis_i = sum(np.log(x_axis_i))/len(x_axis_i)
     av_cv_i = sum(np.log(cv[i-1]))/len(cv[i-1])
 
@@ -65,16 +67,21 @@ plt.rc('text', usetex=True)
 
 plt.xscale('log')
 plt.yscale('log')
-colors = ['green','cyan','darkviolet'] # set the colors
-labels = [r'$\phi=1.0$', r'$\phi=2.67$',r'$\phi=5.0$'] # set the labels of CV
+colors = ['green','cyan','darkviolet'] # for Fig. 4.2(a),(b)
+        # if you want Fig. 4.1(a),(b): 
+        # colors = ['mediumturquoise','red','blue']
+labels = [r'$\phi=1.0$', r'$\phi=2.67$',r'$\phi=5.0$'] # for Fig. 4.2(a),(b)
+        # if you want Fig. 4.1(a),(b): 
+        # labels = [r'$F=0.5$', r'$F=0.7$',r'$F=0.9$']
 
 for i in range(3):
     plt.plot(x_axis_cv[i], cv[i],'.',color=colors[i],label=labels[i])
-    plt.plot\
-      (x_axis_regression, regression[i],'-',color=colors[i],alpha = 0.3)
+    plt.plot(x_axis_regression, regression[i],'-',color=colors[i],alpha = 0.3)
 
-plt.ylabel(r'$VK \; [{\rm \%}]$', fontsize=12)
-plt.xlabel(r'$Fs_i$', fontsize=12)
+plt.ylabel(r'$\overline{VK_i} \; [{\rm \%}]$', fontsize=12)
+plt.xlabel(r'$w_i^\infty$', fontsize=12) # for Fig. 4.1,4.2(b) 
+        # if you want Fig. 4.1,4.2(a): 
+        # plt.xlabel(r'$s_i$', fontsize=12)
 plt.legend(loc=1, fontsize=12)
 plt.show()
 # set the name of the file here
